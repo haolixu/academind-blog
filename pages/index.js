a2f37affec746ac97c96bc28a5fed94b26bbc6b7
黎旭 <1300311464@qq.com>
2022-03-14 11:13:46 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -18,8 +18,8 @@ export default function Home({posts}) {
   return (
     <>
       <Head>
-        <title>Tech Blog</title>
-        <meta name="description" content="Keep up to date with the latest trends in tech" />
+        <title>nb404</title>
+        <meta name="description" content="nb404.cn" />
         <link rel="icon" href="/favicon.ico" />
         {/* You can add more metadata here, like open graph tags for social media, etc */}
       </Head>
@@ -28,7 +28,7 @@ export default function Home({posts}) {
         <h1 className="text-center pb-5">Tech Blog</h1>
         <div className="row">
           <div className="col-lg-8">
-            <h2 className="pb-3">Our blog posts</h2>
+            <h2 className="pb-3">coolman的日常</h2>
             {jsxPosts}
           </div>
          
