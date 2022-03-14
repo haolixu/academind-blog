f39e94278a36433da1ddbc767e0057c265763db0
黎旭 <1300311464@qq.com>
2022-03-14 10:12:58 +0800

    Update wordpress.js

--- utils/wordpress.js
+++ utils/wordpress.js
@@ -5,7 +5,6 @@ export async function getPosts() {
   return posts;
 }
 
-
 export async function getPost(slug) {
   const posts = await getPosts();
   const postArray = posts.filter((post) => post.slug == slug);
